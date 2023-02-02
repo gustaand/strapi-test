4b81799fa436a1f61e31e91e6e7e1744ca99f9e4
Gustavo Andrew <gusta.and21@gmail.com>
2023-02-03 00:52:19 +0100

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+      apiToken: "8cuE3xH7OpJRqe5VRPLqJGdS",
+      appFilter: "strapi-test",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
